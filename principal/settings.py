4815fcee21617eb3faf0abbcd9ab4388651c6237
Igo <igofer1435@gmail.com>
2026-02-03 11:36:55 -0300

    update de caminhos

--- principal/settings.py
+++ principal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$(zq2tnly8714*^=qje0#frho5c9--k#%gletm*sx)3t0*t@wz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [igofer1435.pythonanywhere.com]
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
